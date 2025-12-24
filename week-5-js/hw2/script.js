089a314b9b1260e18fd9a33faeca8aefbd9ac5c1
Beyza Oz <beyzanuroz_@hotmail.com>
2025-12-23 18:47:41 -0600

    drumkit

---
+++ week-5-js/hw2/script.js
@@ -0,0 +1,60 @@
+
+let audio0 = document.getElementById("audio");
+document.getElementById('button').addEventListener('click',()=>{
+   audio0.play();
+});
+
+
+let audio1 = document.getElementById("audio1");
+document.getElementById('button1').addEventListener('click',()=>{
+    audio1.play();
+});
+
+
+let audios2 = document.getElementById("audio2");
+document.getElementById('button2').addEventListener('click',()=>{
+    audios2.play();
+});
+
+let audio3 =document.getElementById("audio3");
+document.getElementById('button3').addEventListener('click', ()=>{
+    audio3.play();
+
+});
+
+let audio4 =document.getElementById("audio4");
+document.getElementById('button4').addEventListener('click', ()=>{
+    audio4.play();
+
+});
+
+let audio5 =document.getElementById("audio5");
+document.getElementById('button5').addEventListener('click', ()=>{
+    audio5.play();
+
+});
+
+let audio6 =document.getElementById("audio6");
+document.getElementById('button6').addEventListener('click', ()=>{
+    audio6.play();
+
+});
+
+let audio7 =document.getElementById("audio7");
+document.getElementById('button7').addEventListener('click', ()=>{
+    audio7.play();
+
+});
+
+let audio8 =document.getElementById("audio8");
+document.getElementById('button8').addEventListener('click', ()=>{
+    audio8.play();
+
+});
+
+
+
+
+
+
+
